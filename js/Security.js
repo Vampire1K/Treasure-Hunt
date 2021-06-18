aac5fafc417d499519c5367544f8cd511c21a7ee
Katherine Pierce <77492568+Vampire1K@users.noreply.github.com>
2021-06-18 10:48:44 +0530

    Add files via upload

--- js/Security.js
+++ js/Security.js
@@ -8,9 +8,19 @@ class Security {
            this.access1.style('background', 'white');
 
 
+           this.button1 = createButton('check');
+           this.button1.position(100,120);
+           this.button1.style('background', 'lightgrey');
+   
+
+
            this.access2 = createInput("code1");
            this.access2.position(100,290);
            this.access2.style('background', 'white');
+
+           this.button2 = createButton('check');
+           this.button2.position(100,323);
+           this.button2.style('background', 'lightgrey');
 
 
 
@@ -18,15 +28,18 @@ class Security {
            this.access3.position(700,190);
            this.access3.style('background', 'white');
 
+           
+           this.button3 = createButton('check');
+           this.button3.position(700,223);
+           this.button3.style('background', 'lightgrey');
+
+
     }
 
     display(){
 
         // Add code to make the buttons function as expecte
-        this.button1 = createButton('check');
-        this.button1.position(100,120);
-        this.button1.style('background', 'lightgrey');
-
+       
 
         this.button1.mousePressed(() => {
             if(system.authentication(accessCode1,this.access1.value())){
@@ -39,9 +52,7 @@ class Security {
 
 
 
-        this.button2 = createButton('check');
-        this.button2.position(100,323);
-        this.button2.style('background', 'lightgrey');
+        
 
 
         this.button2.mousePressed(() => {
@@ -55,10 +66,6 @@ class Security {
 
 
 
-
-        this.button3 = createButton('check');
-        this.button3.position(700,223);
-        this.button3.style('background', 'lightgrey');
 
 
         this.button3.mousePressed(() => {
